056d77a56adcdbe743bfd397ad025cffafb17258
alyssabrande <102975794+alyssabrande@users.noreply.github.com>
2022-04-04 08:27:43 -0600

    added something to "forever"

--- main.ts
+++ main.ts
@@ -3,5 +3,8 @@ input.onButtonPressed(Button.A, function () {
 })
 basic.showIcon(IconNames.Umbrella)
 basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+basic.forever(function () {
 	
 })
