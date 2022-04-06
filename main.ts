df03292758813cd78df0f29a902d61e7963197e6
alyssabrande <102975794+alyssabrande@users.noreply.github.com>
2022-04-05 21:56:04 -0600

    added an icon for when button A is pressed

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Alyssa Brande")
+    basic.showIcon(IconNames.Snake)
 })
 basic.showIcon(IconNames.Umbrella)
 basic.forever(function () {
